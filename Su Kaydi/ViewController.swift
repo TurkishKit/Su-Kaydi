9ad1d6d989550a06e9e50c593a057aa055ce626f
Ege Sucu <egesucu@icloud.com>
2019-11-30 14:13:55 +0300

    Initial Commit

---
+++ Su Kaydi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Su Kaydi
+//
+//  Created by Ege Sucu on 30.11.2019.
+//  Copyright © 2019 Softhion. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
